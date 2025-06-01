da7025dd3bc343dd840b3593a1c2a8e025b8446f
felix <rraefelix@gmail.com>
2025-06-01 15:19:15 +0800

    second try to fix the express version problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get("/*path", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
